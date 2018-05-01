6e42628b0dd17b54ea68170f0409abdd090c0af2
chantzish <chantzish@gmail.com>
2018-05-01 17:52:47 +0300

    first ex.

--- public/script.js
+++ public/script.js
@@ -5,24 +5,35 @@ var updateCart = function () {
   // TODO: Write this function. In this function we render the page.
   // Meaning we make sure that all our cart items are displayed in the browser.
   // Remember to empty the "cart div" before you re-add all the item elements.
+  $('.cart-list').empty() //empty // .html("")
+  cart.forEach(item => {$('.cart-list').append("<p>" + item.name + " - $" + item.price + "</p>")});
+  $('.total').text(cart.reduce((acc, item) => acc + item.price, 0));
 }
 
 
 var addItem = function (item) {
   // TODO: Write this function. Remember this function has nothing to do with display. 
   // It simply is for adding an item to the cart array, no HTML involved - honest ;-)
+  cart.push(item);
 }
 
 var clearCart = function () {
   // TODO: Write a function that clears the cart ;-)
+  cart = [];
+  updateCart();
 }
 
 $('.view-cart').on('click', function () {
   // TODO: hide/show the shopping cart!
+  $('.shopping-cart').toggle();
 });
 
 $('.add-to-cart').on('click', function () {
   // TODO: get the "item" object from the page
+  var item = {
+    name: $(this).closest('.card.item').data().name,
+    price: $(this).closest('.card.item').data().price
+  }
   addItem(item);
   updateCart();
 });
